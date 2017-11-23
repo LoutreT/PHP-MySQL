4cd1140b61abef217e2affa9bdac4f457a1387fe
Vincent <32301427+LoutreT@users.noreply.github.com>
2017-11-23 16:37:54 +0100

    Update index.php
    ajout pdo

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-echo"Climb-Oxb"
+try
+{
+    // On se connecte à MySQL
+    $pdo = new PDO('pgsql:host=ec2-46-137-174-67.eu-west-1.compute.amazonaws.com;dbname=dd76qk94md6on7;', 'nflgkofauvtovx', 'd1c8a82598d9fa7cdf92926e70ca55596ddc799b85b16a14f4d30003f427d790');
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+$query = 'INSERT INTO categories (nom, description) VALUES (?, ?);';
+$prep = $pdo->prepare($query);
+ 
+$prep->bindValue(1, 'bertand', PDO::PARAM_STR);
+$prep->bindValue(2, 'ceci est un test pour desc', PDO::PARAM_STR);
+$prep->execute();
+$resultat = $pdo->query('SELECT * FROM categories');
+while ($donnees = $resultat->fetch())
+{
+  echo '<br/>';
+  echo $donnees['nom'];
+  echo ' : ';
+  echo $donnees['description'];
+}
+
 ?>
